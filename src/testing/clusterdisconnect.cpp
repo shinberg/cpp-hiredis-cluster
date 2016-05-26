88fa9c3754fc8a0de963140fa4a58dde5d737178
Дмитрий <shinmail@gmail.com>
2016-05-26 07:58:43 +0400

    added source file

---
+++ src/testing/clusterdisconnect.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <event2/event.h>
+#include <signal.h>
+#include <unistd.h>
+
+#include "asynchirediscommand.h"
+
+using RedisCluster::AsyncHiredisCommand;
+using RedisCluster::Cluster;
+
+using std::string;
+using std::out_of_range;
+using std::cerr;
+using std::cout;
+using std::endl;
+
+static void setCallback( typename Cluster<redisAsyncContext>::ptr_t cluster_p, void *r, void *data )
+{
+    redisReply * reply = static_cast<redisReply*>( r );
+    string *demoData = static_cast<string*>( data );
+    
+    if (!reply) {
+        cout << "empty reply!" << endl;
+        return;
+    }
+    
+    if( reply->type == REDIS_REPLY_STATUS || reply->type == REDIS_REPLY_ERROR )
+    {
+        cout << " Reply to SET FOO BAR " << endl;
+        cout << reply->str << endl;
+    }
+    
+    cout << *demoData << endl;
+    delete demoData;
+    // cluster disconnect must be invoked, instead of redisAsyncDisconnect
+    // this will brake event loop
+    //cluster_p->disconnect();
+}
+
+void processAsyncCommand()
+{
+    Cluster<redisAsyncContext>::ptr_t cluster_p;
+    
+    signal(SIGPIPE, SIG_IGN);
+    struct event_base *base = event_base_new();
+    
+    cluster_p = AsyncHiredisCommand<>::createCluster( "192.168.33.10", 7000, static_cast<void*>( base ) );
+    
+    while (true) {
+        string *demoData = new string("Demo data is ok");
+        AsyncHiredisCommand<>::Command( cluster_p,
+                                           "FOO",
+                                           setCallback,
+                                           static_cast<void*>( demoData ),
+                                           "SET %s %s",
+                                            "FOO",
+                                           "BAR1" );
+            
+        event_base_loop(base, EVLOOP_NONBLOCK | EVLOOP_ONCE);
+        usleep(1000);
+    }
+    
+    event_base_dispatch(base);
+    delete cluster_p;
+    event_base_free(base);
+}
+
+int main(int argc, const char * argv[])
+{
+    try
+    {
+        processAsyncCommand();
+    } catch ( const RedisCluster::ClusterException &e )
+    {
+        cout << "Cluster exception: " << e.what() << endl;
+    }
+    return 0;
+}
+
